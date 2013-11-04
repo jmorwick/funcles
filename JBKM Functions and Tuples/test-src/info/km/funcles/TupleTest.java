1b4f5c68b675ec18cc2ae7cd385e6792c64786ed
jmorwick <jmorwick@192.168.0.193>
2013-11-04 00:22:53 -0500

    started building in junit - mostly checking to see if I have git/eclipse set up properly

---
+++ JBKM Functions and Tuples/test-src/info/km/funcles/TupleTest.java
@@ -0,0 +1,59 @@
+package info.km.funcles;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TupleTest {
+
+	@Test
+	public void testTuple() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMutable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeTupleAB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeTupleABC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeTupleABCD() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeTupleABCDE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeImmutableTupleAB() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeImmutableTupleABC() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeImmutableTupleABCD() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakeImmutableTupleABCDE() {
+		fail("Not yet implemented");
+	}
+
+}
