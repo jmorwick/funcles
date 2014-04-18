320f2f86658f52f93dfe8b920968d6d6f7404f8e
jmorwick <jmorwick@192.168.0.105>
2014-04-18 11:50:26 -0400

    removed planned tests for removed features

--- JBKM Functions and Tuples/test-src/info/km/funcles/PredicatesTest.java
+++ JBKM Functions and Tuples/test-src/info/km/funcles/PredicatesTest.java
@@ -9,25 +9,6 @@ import org.junit.Test;
 public class PredicatesTest {
 
 	@Test
-	public void testFilter() {
-		fail("Not yet implemented");
-	}
-
-
-	@Test
-	public void testPartition1() {
-		Set<Integer> nums = 
-	}
-
-
-
-	@Test
-	public void testPartition2() {
-		fail("Not yet implemented");
-	}
-
-
-	@Test
 	public void testArgmax() {
 		fail("Not yet implemented");
 	}
