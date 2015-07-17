b3fb6e61346d36418dea61a9abd95484b008f1df
jmorwick <jbmorwick@gmail.com>
2015-07-17 01:08:11 -0500

    removed memoization
    moved memorization code to its own tiny project. Move the memorization
    example out of the project as well (in a local examples repository now)

--- test-src/net/sourcedestination/funcles/FunclesTests.java
+++ test-src/net/sourcedestination/funcles/FunclesTests.java
@@ -28,7 +28,6 @@ import java.util.Set;
 import java.util.function.Function;
 
 import net.sourcedestination.funcles.Funcles;
-import net.sourcedestination.funcles.MemoizedFunction;
 import net.sourcedestination.funcles.relation.Relation2;
 import net.sourcedestination.funcles.relation.Relation3;
 import net.sourcedestination.funcles.tuple.Pair;
@@ -127,38 +126,4 @@ public class FunclesTests {
 	}
 
 
-
-	@Test
-	public void testMemoize() {
-		Function<Integer,Integer> f = new Function<Integer,Integer>() {
-
-			@Override
-			public Integer apply(Integer x) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return x;
-			}
-			
-		};
-		
-		MemoizedFunction<Integer, Integer> mf = MemoizedFunction.memoize(f);
-		long time = System.currentTimeMillis();
-		assertEquals((Integer)5, mf.apply(5));
-		assertTrue(System.currentTimeMillis() >= time + 100);
-		time = System.currentTimeMillis();
-		assertEquals(mf.apply(5), (Integer)5);
-		assertTrue(System.currentTimeMillis() < time + 100);
-		time = System.currentTimeMillis();
-		assertEquals(mf.apply(10), (Integer)10);
-		assertTrue(System.currentTimeMillis() >= time + 100);
-		time = System.currentTimeMillis();
-		assertEquals(mf.apply(10), (Integer)10);
-		assertTrue(System.currentTimeMillis() < time + 100);
-	}
-
-	
 }
